62e42189bffa0d892d9fecee2914ed64549a9fa7
phptuts <glaserpower@gmail.com>
2021-09-13 22:56:47 -0700

    got navbar working

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,32 +1,49 @@
 <script lang="ts">
-	import { Row, Col } from 'sveltestrap';
-	import firebase from 'firebase/app';
+  import { Row, Col } from 'sveltestrap';
+  import firebase from 'firebase/app';
+  import { goto } from '$app/navigation';
+  import { onDestroy } from 'svelte';
+  import authStore from '../stores/authStore';
 
-	async function loginWithGoogle() {
-		try {
-			const provider = new firebase.auth.GoogleAuthProvider();
+  async function loginWithGoogle() {
+    try {
+      const provider = new firebase.auth.GoogleAuthProvider();
 
-			await firebase.auth().signInWithPopup(provider);
-		} catch (e) {
-			console.log(e);
-		}
-	}
+      await firebase.auth().signInWithPopup(provider);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const sub = authStore.subscribe(async (info) => {
+    if (info.isLoggedIn) {
+      await goto('/');
+    }
+  });
+
+  onDestroy(() => {
+    sub();
+  });
 </script>
 
 <Row>
-	<Col>
-		<h1>Login with Google</h1>
-	</Col>
+  <Col>
+    <h1>Login with Google</h1>
+  </Col>
 </Row>
 
 <Row>
-	<Col>
-		<img on:click={loginWithGoogle} src="/login-with-google.png" alt="Login With Google" />
-	</Col>
+  <Col>
+    <img
+      on:click={loginWithGoogle}
+      src="/login-with-google.png"
+      alt="Login With Google"
+    />
+  </Col>
 </Row>
 
 <style>
-    img {
-        cursor: pointer;
-    }
+  img {
+    cursor: pointer;
+  }
 </style>
